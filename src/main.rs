8796c9ac08605f72c729bd329ce96b86d36d910a
dfebs <daniel.febles97@gmail.com>
2026-04-15 22:31:53 -0400

    Get the very first byte of an image

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
     get_text_from_file("hello.txt");
+    let data: Vec<u8> = match fs::read("gpru.png") {
+        Err(why) => panic!("{}", why),
+        Ok(bytes) => bytes,
+    };
+
+    let first_byte = data[0].to_string();
+
+    // How do I iterate through all the bytes and print them?
+    // How do I get the size of the byte array?
+    // How does that compare to the computer's interpretation of the file size?
+    // What does it look like when all the things are printed?
+    // Find some time to look into the png spec.
+
+    println!("{}", first_byte);
 }
 
 fn get_text_from_file(path: &str) {
