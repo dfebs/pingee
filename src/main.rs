7c0ba2df8795f1b13b7262e912b20d470af3824d
dfebs <daniel.febles97@gmail.com>
2026-04-16 09:04:46 -0400

    Get a separate module working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+
+mod text_tools;
+use text_tools::file_handler::get_text_from_file;
 
 fn main() {
     get_text_from_file("hello.txt");
@@ -19,30 +19,4 @@ fn main() {
     // Find some time to look into the png spec.
 
     println!("{}", first_byte);
-}
-
-fn get_text_from_file(path: &str) {
-    let file = open_file(path);
-    let text = read_text_file(path, &file);
-
-    println!("{}", &text);
-}
-
-fn open_file(path: &str) -> File {
-    let path = Path::new(path);
-    let display = path.display();
-
-    match File::open(&path) {
-        Err(why) => panic!("could not open {}: {}", display, why),
-        Ok(file) => file,
-    }
-}
-
-fn read_text_file(path: &str, mut file: &File) -> String {
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", path, why),
-        Ok(_) => print!("Successfully received file\n"),
-    }
-    s
 }
---
+++ src/text_tools.rs
@@ -0,0 +1,31 @@
+pub mod file_handler {
+    use std::fs::File;
+    use std::io::Read;
+    use std::path::Path;
+
+    pub fn open_file(path: &str) -> File {
+        let path = Path::new(path);
+        let display = path.display();
+
+        match File::open(&path) {
+            Err(why) => panic!("could not open {}: {}", display, why),
+            Ok(file) => file,
+        }
+    }
+
+    fn read_text_file(path: &str, mut file: &File) -> String {
+        let mut s = String::new();
+        match file.read_to_string(&mut s) {
+            Err(why) => panic!("couldn't read {}: {}", path, why),
+            Ok(_) => print!("Successfully received file\n"),
+        }
+        s
+    }
+
+    pub fn get_text_from_file(path: &str) {
+        let file = open_file(path);
+        let text = read_text_file(path, &file);
+
+        println!("{}", &text);
+    }
+}
