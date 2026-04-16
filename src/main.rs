47d6dbb9c6cdc8a74b8699139f1acf31ae99b42b
dfebs <daniel.febles97@gmail.com>
2026-04-15 22:09:19 -0400

    Refactor main file to consist of separate functions

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,31 @@ use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("hello.txt");
+    get_text_from_file("hello.txt");
+}
+
+fn get_text_from_file(path: &str) {
+    let file = open_file(path);
+    let text = read_text_file(path, &file);
+
+    println!("{}", &text);
+}
+
+fn open_file(path: &str) -> File {
+    let path = Path::new(path);
     let display = path.display();
 
-    let mut file = match File::open(&path) {
+    match File::open(&path) {
         Err(why) => panic!("could not open {}: {}", display, why),
         Ok(file) => file,
-    };
+    }
+}
 
+fn read_text_file(path: &str, mut file: &File) -> String {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains:\n{}", display, s),
+        Err(why) => panic!("couldn't read {}: {}", path, why),
+        Ok(_) => print!("Successfully received file\n"),
     }
-
-    println!("{}", s);
+    s
 }
