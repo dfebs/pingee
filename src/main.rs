b2e9e3bbd8196d0ba4e1f13cb0090f672b9635b1
dfebs <daniel.febles97@gmail.com>
2026-04-19 11:01:05 -0400

    Find the IEND

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ fn main() {
         Ok(bytes) => bytes,
     };
 
-    let sequences = find_sequences(&bytes, &vec![137, 80, 78, 71, 13, 10, 26, 10]);
-    for item in sequences.iter() {
-        println!("{}", item);
+    println!("Bytes length: {}", bytes.len());
+
+    let beginning = find_sequences(&bytes, &vec![137, 80, 78, 71, 13, 10, 26, 10]);
+    for item in beginning.iter() {
+        println!("Beginning bytes located at {}", item);
+    }
+
+    let iend = find_sequences(&bytes, &vec![0x49, 0x45, 0x4e, 0x44]);
+    for &item in iend.iter() {
+        println!("IEND located at {}", item);
+        let slice = &bytes[item..];
+        println!("Last of bits starting at IEND: {:02X?}", slice)
+        // TODO: What are those last 4 bytes?
     }
 }
