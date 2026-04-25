2f4b00275f31f85c09e5ecf7a37027a8c71cb89d
dfebs <daniel.febles97@gmail.com>
2026-04-25 19:48:41 -0400

    Use a leaner version of gpru.png

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use pingee::png_tools::png_tools::*;
 use std::fs;
 
 fn main() {
-    let bytes: Vec<u8> = match fs::read("gpru.png") {
+    let bytes: Vec<u8> = match fs::read("gpru_lean.png") {
         Err(why) => panic!("{}", why),
         Ok(bytes) => bytes,
     };
