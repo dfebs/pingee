841d7318dd40f76b071de65f4dba653bb6f7c88a
dfebs <daniel.febles97@gmail.com>
2026-04-27 19:10:14 -0400

    Add image data to PNG struct

--- src/png_model/mod.rs
+++ src/png_model/mod.rs
@@ -1,12 +1,14 @@
 pub mod chunks;
 pub mod png_model {
     use crate::{png_model::chunks::chunks::*, png_tools::png_tools::*};
+    use std::collections::HashMap;
 
     #[derive(Debug)]
     pub struct Png {
         // Found in image header
         pub header: Header,
         pub palette: Option<Vec<Color>>,
+        pub img_data: Vec<u8>,
     }
 
     impl Png {
@@ -14,8 +16,13 @@ pub mod png_model {
             let chunks = get_chunks(&bytes);
             let header = Self::retrieve_headers(chunks.get("IHDR"));
             let palette = Self::retrieve_palette(chunks.get("PLTE"));
+            let img_data = Self::retrieve_image_data(&chunks);
 
-            Png { header, palette }
+            Png {
+                header,
+                palette,
+                img_data,
+            }
         }
 
         fn retrieve_headers(chunk: Option<&Chunk>) -> Header {
@@ -46,8 +53,7 @@ pub mod png_model {
             if palette.len() % 3 != 0 {
                 panic!("Palette is not divisible by 3. It may be corrupted.");
             }
-            // Figure out how to chunk up the palette into groups of 3
-            // let colors = palette.as_chunks(3);
+
             let colors: Vec<Color> = palette
                 .chunks_exact(3)
                 .map(|color| Color(color[0], color[1], color[2]))
@@ -56,8 +62,13 @@ pub mod png_model {
             Some(colors)
         }
 
-        fn retrieve_image_data() {
-            todo!();
+        fn retrieve_image_data(chunks: &HashMap<String, Chunk>) -> Vec<u8> {
+            chunks
+                .iter()
+                .filter(|(key, _)| key.contains("IDAT"))
+                .map(|(_, value)| value.data.to_owned())
+                .flatten()
+                .collect()
         }
     }
 }
